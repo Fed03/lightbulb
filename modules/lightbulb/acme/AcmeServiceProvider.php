2552049daf09c37fb4644f8e67316ce1e38a27e1
crynobone <crynobone@gmail.com>
2014-01-22 22:15:18 +0800

    Add basic test environment.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ modules/lightbulb/acme/AcmeServiceProvider.php
@@ -0,0 +1,11 @@
+<?php namespace Lightbulb\Acme;
+
+use Illuminate\Support\ServiceProvider;
+
+class AcmeServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+}
